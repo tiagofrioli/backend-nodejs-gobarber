4378055e2a3385e71e675ced717c24e9803bcf97
tiagofrioli <tiago.frioli@gmail.com>
2020-05-30 00:45:42 -0300

    Autenticação - Habilitando CORS na API

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import cors from 'cors';
+
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes';
@@ -7,6 +9,8 @@ import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
